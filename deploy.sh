de1dbf23629b0b21ea86d9c96b369f7f8925953f
Kinezumi <kinezumi91@gmail.com>
2017-11-04 14:07:54 +0200

    next version

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
+git pull;
 git add .;
 git commit -m "next version";
 git push;
 mvn clean;
-mvn package;
+mvn package scp /c/Users/jusju/git/joulukortit2/target/joulukortit2.war jusju@palvelum.me:/var/lib/tomcat8/webapps:
