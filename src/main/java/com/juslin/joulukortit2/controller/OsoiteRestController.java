c2fa91e702f240990fb338a15023d5efea3d5bff
Kinezumi <kinezumi91@gmail.com>
2017-11-11 18:12:27 +0200

    next version

---
+++ src/main/java/com/juslin/joulukortit2/controller/OsoiteRestController.java
@@ -0,0 +1,29 @@
+package com.juslin.joulukortit2.controller;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.juslin.joulukortit2.bean.Osoite;
+import com.juslin.joulukortit2.dao.OsoiteDAO;
+
+@Controller
+public class OsoiteRestController {
+	@Inject
+	OsoiteDAO dao;
+
+	@RequestMapping("henkilot.json")
+	public @ResponseBody List<Osoite> haeHenkilotJSON() {
+		List<Osoite> henkilot = dao.haeKaikki();
+		return henkilot;
+	}
+	
+	@RequestMapping("henkilot")
+	public String naytaOSoitelista() {
+		return "henk/henkilolista";
+	}
+}
