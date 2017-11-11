4d6dfc4f0d7c37e6ff73831817af2c5cfc3edb27
Kinezumi <kinezumi91@gmail.com>
2017-11-11 13:27:08 +0200

    next version

--- src/main/java/com/juslin/joulukortit2/servlet/KirjautumisController.java
+++ src/main/java/com/juslin/joulukortit2/servlet/KirjautumisController.java
@@ -2,9 +2,11 @@ package com.juslin.joulukortit2.servlet;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.juslin.joulukortit2.bean.HenkiloImpl;
 import com.juslin.joulukortit2.bean.Kayttaja;
 
 @Controller
@@ -13,10 +15,17 @@ public class KirjautumisController {
 	
 	//FORMIN TIETOJEN VASTAANOTTO
 	@RequestMapping(value="kirjaudu", method=RequestMethod.GET)
-	public String kirjautumisLogiikka(Model model) {
+	public String naytaKirjautuminen(Model model) {
 		Kayttaja kayttaja = new Kayttaja();
 		model.addAttribute("kayttaja", kayttaja);
 		System.out.println("KirjautumisController.kirjautumisLogiikka()");
+		return "kirjautuminen";
+	}
+	
+	//FORMIN TIETOJEN VASTAANOTTO
+	@RequestMapping(value="kirjaudu", method=RequestMethod.POST)
+	public String kirjautumisPaatos(@ModelAttribute(value="kayttaja") Kayttaja kayttaja) {
+		System.out.println("KirjautumisController.kirjautumisPaatos()");
 		return "kirjautuminen";
 	}
 }
